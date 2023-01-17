104723c4a686ff07597a7da96a4f167ee6cb0d55
Abdelouahed  Rabiai <arabiai@e2r3p1.1337.ma>
2023-01-17 17:33:47 +0100

    fixed some leaks

---
+++ mandat/errors_utils.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errors_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arabiai <arabiai@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/14 20:45:23 by arabiai           #+#    #+#             */
+/*   Updated: 2023/01/17 17:19:55 by arabiai          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	error_handling(int x)
+{
+	if (x == 0)
+		ft_printf("EROOR : invalid number of arguments \n");
+	else if (x == 1)
+		ft_printf("EROOR creating the pipe \n");
+	else if (x == 2)
+		ft_printf("EROOR forking the process \n");
+	exit(EXIT_FAILURE);
+}
+
+void	errorfile_free(int x, char **strs, char **splited_paths, char *cmd)
+{
+	if (x == 0)
+		ft_printf("EROOR : opening the infile\n");
+	else if (x == 1)
+		ft_printf("EROOR : opening the outfile\n");
+	else if (x == 2)
+	{
+		ft_printf("ERROR : %s command not found \n", cmd);
+		ft_free_split(strs);
+		ft_free_split(splited_paths);
+		exit(EXIT_FAILURE);
+	}
+	else if (x == 3)
+	{
+		ft_printf("ERROR : %s command not found : All commands not available \n", cmd);
+		ft_free_split(strs);
+		ft_free_split(splited_paths);
+		exit(EXIT_FAILURE);
+	}
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*strjoin;
+	size_t	i;
+
+	i = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	strjoin = (char *)malloc(sizeof(char)
+			* (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!strjoin)
+		return (NULL);
+	while (i < ft_strlen(s1))
+	{
+		strjoin[i] = s1[i];
+		i++;
+	}
+	while (i < ft_strlen(s1) + ft_strlen(s2))
+	{
+		strjoin[i] = s2[i - ft_strlen(s1)];
+		i++;
+	}
+	strjoin[i] = '\0';
+	return (strjoin);
+}
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	char	*hay;
+	char	*nee;
+	size_t	i;
+	size_t	j;
+
+	if ((!haystack && !len))
+		return (NULL);
+	hay = (char *)haystack;
+	nee = (char *)needle;
+	i = 0;
+	j = 0;
+	if (nee[j] == '\0')
+		return (hay);
+	while (hay[i] != '\0' && i < len)
+	{
+		j = 0;
+		while (nee[j] == hay[i + j] && hay[i + j] != '\0' && (i + j) < len)
+			j++;
+		if (nee[j] == '\0')
+			return (hay + i);
+		i++;
+	}
+	return (0);
+}
---
+++ mandat/pipex_main.c
@@ -0,0 +1,125 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_main.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arabiai <arabiai@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/23 13:28:55 by arabiai           #+#    #+#             */
+/*   Updated: 2023/01/17 17:25:26 by arabiai          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+char	**get_envpath(char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i] != NULL && ft_strnstr(envp[i], "PATH", 4) == NULL)
+		i++;
+	if (envp[i] == NULL)
+		return (NULL);
+	return (ft_split(envp[i] + 5, ':'));
+}
+
+char	*get_command_path(char **paths, char **main_cmd)
+{
+	int		i;
+	char	*str;
+	char	*cmd;
+
+	i = 0;
+	cmd = ft_strjoin("/", main_cmd[0]);
+	if (paths == NULL)
+		return (free(cmd),
+			errorfile_free(3, main_cmd, paths, main_cmd[0]), NULL);
+	if (access(main_cmd[0], F_OK) == 0 && access(main_cmd[0], X_OK) == 0)
+		return (free(cmd), ft_free_split(paths), main_cmd[0]);
+	while (paths[i] != NULL)
+	{
+		str = ft_strjoin(paths[i], cmd);
+		if (access(str, F_OK) == 0)
+			break ;
+		i++;
+		// free(str);
+	}
+	if (paths[i] == NULL)
+		return (free(cmd),
+			errorfile_free(2, main_cmd, paths, main_cmd[0]), NULL);
+	else
+		return (free(cmd), ft_free_split(paths), str);
+}
+
+void	child_process(char **argv, int pipe_ends[2], char **envp)
+{
+	int		fd_in;
+	char	*path;
+	char	**strs;
+	char	**splited_paths;
+
+	splited_paths = get_envpath(envp);
+	strs = ft_split(argv[2], ' ');
+	fd_in = open(argv[1], O_RDONLY);
+	if (fd_in < 0)
+		errorfile_free(0, strs, splited_paths, "no_use");
+	path = get_command_path(splited_paths, strs);
+	close(pipe_ends[0]);
+	dup2(fd_in, STDIN_FILENO);
+	dup2(pipe_ends[1], STDOUT_FILENO);
+	close(fd_in);
+	execve(path, strs, envp);
+	exit(EXIT_SUCCESS);
+}
+
+void	child1_process(char **argv, int pipe_ends[2], char **envp)
+{
+	char	*path;
+	char	**strs;
+	int		fd_out;
+	char	**splited_paths;
+
+	splited_paths = get_envpath(envp);
+	strs = ft_split(argv[3], ' ');
+	fd_out = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
+	if (fd_out < 0)
+		errorfile_free(1, strs, splited_paths, "no_use");
+	path = get_command_path(splited_paths, strs);
+	close(pipe_ends[1]);
+	dup2(fd_out, STDOUT_FILENO);
+	dup2(pipe_ends[0], STDIN_FILENO);
+	close(fd_out);
+	execve(path, strs, envp);
+	exit(EXIT_SUCCESS);
+}
+
+// void lek(){
+// 	system("leaks pipex");
+// }
+
+int	main(int ac, char **argv, char **envp)
+{
+	pid_t	pid;
+	pid_t	pid1;
+	int		pipe_ends[2];
+
+	if (ac != 5)
+		error_handling(0);
+	if (pipe(pipe_ends) == -1)
+		error_handling(1);
+	pid = fork();
+	if (pid == -1)
+		error_handling(2);
+	if (pid == 0)
+		child_process(argv, pipe_ends, envp); // execve != close
+	pid1 = fork();
+	if (pid1 == 0)
+		child1_process(argv, pipe_ends, envp);
+	close(pipe_ends[1]);
+	close(pipe_ends[0]);
+	while (wait(NULL) != -1)
+	{
+	}
+	return (0);
+}
